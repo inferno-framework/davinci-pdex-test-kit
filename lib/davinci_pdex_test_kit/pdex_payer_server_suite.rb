7aa2715b9762bbe858ca4f53c9b235b25e95fe23
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-04-15 16:54:40 -0400

    fix indenting for pdex_payer_server_suite.rb

--- lib/davinci_pdex_test_kit/pdex_payer_server_suite.rb
+++ lib/davinci_pdex_test_kit/pdex_payer_server_suite.rb
@@ -1,41 +1,41 @@
 require_relative 'pdex_payer_server/dollar_everything'
 
 module DaVinciPDexTestKit
-    class PDexPayerServerSuite < Inferno::TestSuite
-      id :pdex_payer_server
-      title 'Da Vinci PDex Payer Server Test Suite'
-      description %(
-        # Da Vinci PDex Payer Server Test Suite
+  class PDexPayerServerSuite < Inferno::TestSuite
+    id :pdex_payer_server
+    title 'Da Vinci PDex Payer Server Test Suite'
+    description %(
+      # Da Vinci PDex Payer Server Test Suite
 
-        This suite validates that a payer system can act as a
-        data source for client systems to connect to and retrieve
-        data from. This includes provider systems as well as other
-        payer systems using the payer to payer data retrieval approach.
-        Inferno will act as a client system connecting to the system
-        under test and making requests for data against it.
-      )
-  
-      # These inputs will be available to all tests in this suite
-      input :url,
-            title: 'FHIR Server Base Url'
-  
-      input :credentials,
-            title: 'OAuth Credentials',
-            type: :oauth_credentials,
-            optional: true
-  
-      # All FHIR requests in this suite will use this FHIR client
-      fhir_client do
-        url :url
-        oauth_credentials :credentials
-      end
-  
-      # All FHIR validation requsets will use this FHIR validator
-      validator do
-        url ENV.fetch('VALIDATOR_URL')
-      end
+      This suite validates that a payer system can act as a
+      data source for client systems to connect to and retrieve
+      data from. This includes provider systems as well as other
+      payer systems using the payer to payer data retrieval approach.
+      Inferno will act as a client system connecting to the system
+      under test and making requests for data against it.
+    )
 
-      group from: :dollar_everything
+    # These inputs will be available to all tests in this suite
+    input :url,
+          title: 'FHIR Server Base Url'
 
+    input :credentials,
+          title: 'OAuth Credentials',
+          type: :oauth_credentials,
+          optional: true
+
+    # All FHIR requests in this suite will use this FHIR client
+    fhir_client do
+      url :url
+      oauth_credentials :credentials
     end
+
+    # All FHIR validation requsets will use this FHIR validator
+    validator do
+      url ENV.fetch('VALIDATOR_URL')
+    end
+
+    group from: :dollar_everything
+
   end
+end
