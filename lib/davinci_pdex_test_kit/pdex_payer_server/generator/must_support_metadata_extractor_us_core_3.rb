4d1f54376269a438928c51506cd7652d04ff062a
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-04-15 16:48:56 -0400

    import us core test kit generator and rename namespace

---
+++ lib/davinci_pdex_test_kit/pdex_payer_server/generator/must_support_metadata_extractor_us_core_3.rb
@@ -0,0 +1,108 @@
+module DaVinciPDexTestKit::PDexPayerServer
+  class Generator
+    class MustSupportMetadataExtractorUsCore3
+      attr_accessor :profile, :must_supports
+
+      def initialize(profile, must_supports)
+        self.profile = profile
+        self.must_supports = must_supports
+      end
+
+      def handle_special_cases
+        add_must_support_choices
+        update_patient_previous_name_address
+        remove_document_reference_custodian
+      end
+
+      def add_must_support_choices
+        choices = []
+
+        case profile.type
+        when 'Device'
+          choices << { paths: ['udiCarrier.carrierAIDC', 'udiCarrier.carrierHRF'] }
+        when 'DocumentReference'
+          choices << { paths: ['content.attachment.data', 'content.attachment.url'] }
+        end
+
+        must_supports[:choices] = choices if choices.present?
+      end
+
+      # FHIR-40299 adds USCDI MustSupport choices for:
+      # * address.period.end and address.use,
+      # * name.period.end and name.use
+      def update_patient_previous_name_address
+        return unless profile.type == 'Patient'
+
+        name_period_exists = false
+        name_use_exists = false
+        address_period_exists = false
+        address_use_exists = false
+
+        must_supports[:elements].each do |element|
+          case element[:path]
+          when 'name.period'
+            element[:path] = 'name.period.end'
+            element[:uscdi_only] = true
+            name_period_exists = true
+          when 'name.use'
+            element[:fixed_value] = 'old'
+            element[:uscdi_only] = true
+            name_use_exists = true
+          when 'address.period'
+            element[:path] = 'address.period.end'
+            element[:uscdi_only] = true
+            address_period_exists = true
+          when 'address.use'
+            element[:fixed_value] = 'old'
+            element[:uscdi_only] = true
+            address_use_exists = true
+          end
+        end
+
+        must_supports[:elements] << {
+          path: 'name.period.end',
+          uscdi_only: true
+        } unless name_period_exists
+
+        must_supports[:elements] << {
+          path: 'name.use',
+          fixed_value: 'old',
+          uscdi_only: true
+        } unless name_use_exists
+
+        must_supports[:elements] << {
+          path: 'address.period.end',
+          uscdi_only: true
+        } unless address_period_exists
+
+        must_supports[:elements] << {
+          path: 'address.use',
+          fixed_value: 'old',
+          uscdi_only: true
+        } unless address_use_exists
+
+        must_supports[:choices] ||= []
+
+        must_supports[:choices] << {
+          paths: ['address.period.end', 'address.use'],
+          uscdi_only: true
+        }
+
+        must_supports[:choices] << {
+          paths: ['name.period.end', 'name.use'],
+          uscdi_only: true
+        }
+
+      end
+
+      # US Core clarified that server implmentation is not required to support DocumentReference.custodian (FHIR-28393)
+      def remove_document_reference_custodian
+        return unless profile.type == 'DocumentReference'
+
+        must_supports[:elements].delete_if do |element|
+          element[:path] == 'custodian'
+        end
+      end
+    end
+  end
+end
---
+++ lib/davinci_pdex_test_kit/pdex_payer_server/generator/special_cases.rb
@@ -0,0 +1,67 @@
+module DaVinciPDexTestKit::PDexPayerServer
+  class Generator
+    module SpecialCases
+      RESOURCES_TO_EXCLUDE = {
+        'Location' => ['v311', 'v400', 'v501', 'v610'],
+        'Medication' => ['v311', 'v400', 'v501', 'v610', 'v700_ballot'],
+        'PractitionerRole' => ['v311', 'v400']
+      }.freeze
+
+      PROFILES_TO_EXCLUDE = [
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-survey',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-vital-signs'
+      ].freeze
+
+      OPTIONAL_RESOURCES = [
+        'PractitionerRole',
+        'QuestionnaireResponse'
+      ].freeze
+
+      OPTIONAL_PROFILES = [
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-simple-observation'
+      ].freeze
+
+      NON_USCDI_RESOURCES = {
+        'Encounter' => ['v311', 'v400'],
+        'Location' => ['v311', 'v400', 'v501', 'v610'],
+        'Organization' => ['v311', 'v400', 'v501', 'v610', 'v700_ballot'],
+        'Practitioner' => ['v311', 'v400'],
+        'PractitionerRole' => ['v311', 'v400', 'v501', 'v610', 'v700_ballot'],
+        'Provenance' => ['v311', 'v400', 'v501', 'v610', 'v700_ballot'],
+        'RelatedPerson' => ['v501', 'v610', 'v700_ballot'],
+        'Specimen' => ['v610', 'v700_ballot']
+      }.freeze
+
+      SEARCHABLE_DELAYED_RESOURCES = {
+        'Location' => ['v700_ballot'],
+        'Practitioner' => ['v501', 'v610', 'v700_ballot']
+      }.freeze
+
+      ALL_VERSION_CATEGORY_FIRST_PROFILES = [
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-careplan',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-diagnosticreport-lab',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-diagnosticreport-note',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-clinical-result',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-clinical-test',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-imaging',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-lab',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-screening-assessment',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-sdoh-assessment',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-social-history',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-survey',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-simple-observation'
+      ].freeze
+
+      VERSION_SPECIFIC_CATEGORY_FIRST_PROFILES = {
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-condition-encounter-diagnosis' => ['v610', 'v700_ballot'],
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-condition-problems-health-concerns' => ['v610', 'v700_ballot']
+      }.freeze
+
+      class << self
+        def exclude_group?(group)
+          RESOURCES_TO_EXCLUDE.key?(group.resource) && RESOURCES_TO_EXCLUDE[group.resource].include?(group.reformatted_version)
+        end
+      end
+    end
+  end
+end
