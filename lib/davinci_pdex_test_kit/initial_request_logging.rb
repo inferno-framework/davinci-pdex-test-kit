5a45f95f1def691835537e4a3eab5844e946920e
360dgries <dgriese@mitre.org>
2024-04-25 14:21:07 -0400

    Add Validation test

--- lib/davinci_pdex_test_kit/initial_request_logging.rb
+++ lib/davinci_pdex_test_kit/initial_request_logging.rb
@@ -1,5 +1,8 @@
+require_relative 'client_validation_test.rb'
+
 module DaVinciPDexTestKit
   class PDexInitialRequestLoggingTest < Inferno::Test
+    include DaVinciPDexTestKit::ClientValidationTest
     include URLs
 
     id :initial_request_logging
@@ -10,7 +13,15 @@ module DaVinciPDexTestKit
     input :access_token
 
     run do
-      requests = load_tagged_requests(SUBMIT_TAG)
+      info flattened_all_resources.to_json
+      expected_resources = SET_TO_BE_GATHERED.values.flatten
+      resource_ids = flattened_all_resources.map(&:id)
+
+      collected_resources, missed_resources = expected_resources.partition do |expected_resource|
+        resource_ids.include?(expected_resource)
+      end
+
+      assert missed_resources.empty?, "Unable to find the following expected resources: #{missed_resources.join(', ')}"
     end
   end
 end
