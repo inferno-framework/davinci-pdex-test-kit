23928a7976f3610efd510e7a134ffb580143a7ab
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-06-27 15:39:18 -0400

    clean

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -116,7 +116,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
-    # FactoryBot.definition_file_paths = [ File.expand_path('./factories', __dir__) ]
     FactoryBot.find_definitions
   end
 end
