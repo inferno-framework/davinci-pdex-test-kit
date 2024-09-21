68b318acfe22c6b09342408103c7ff1438ca3176
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-09-21 13:54:35 -0400

    impl multi member match request profile validator

--- lib/davinci_pdex_test_kit/pdex_prior_auth_suite.rb
+++ lib/davinci_pdex_test_kit/pdex_prior_auth_suite.rb
@@ -6,12 +6,18 @@ module DaVinciPDexTestKit
 
     fhir_resource_validator do
       igs 'igs/davinci-pdex-2.1.0.tgz'
+
+
+      cli_context do
+          allowExampleUrls true
+      end
     end
 
     input :explanation_of_benefit,
       type: 'textarea',
       title: 'PDex Prior Authorization instance',
-      description: 'JSON only'
+      description: 'JSON only',
+      optional: true
 
     group do
       id :pdex_prior_auth_group
@@ -24,6 +30,7 @@ module DaVinciPDexTestKit
         input :explanation_of_benefit
   
         run do
+          skip_if !explanation_of_benefit
           assert_valid_json(explanation_of_benefit)
         end
       end
@@ -35,6 +42,7 @@ module DaVinciPDexTestKit
         input :explanation_of_benefit
   
         run do
+          skip_if !explanation_of_benefit
           assert_valid_resource(resource: FHIR.from_contents(explanation_of_benefit))
         end
       end
@@ -46,10 +54,44 @@ module DaVinciPDexTestKit
         input :explanation_of_benefit
   
         run do
+          skip_if !explanation_of_benefit
           assert_valid_resource(resource: FHIR.from_contents(explanation_of_benefit), profile_url: 'http://hl7.org/fhir/us/davinci-pdex/StructureDefinition/pdex-priorauthorization')
         end
       end
     end
+
+    group do
+      id :pdex_multi_member_match_request_group
+      title 'PDex STU 2.1 Multi Member Match Request'
+
+      input :multi_member_match_request,
+        type: 'textarea',
+        title: 'PDex Multi Member Match Request instance',
+        optional: true
+
+      test do
+        title 'Parameters Resource Validation'
+
+        input :multi_member_match_request
+
+        run do
+          skip_if !multi_member_match_request
+          assert_valid_resource(resource: FHIR.from_contents(multi_member_match_request))
+        end
+      end
+
+      test do
+        title 'Member Match Request Profile Validation'
+
+        input :multi_member_match_request
+
+        run do
+          skip_if !multi_member_match_request
+          assert_valid_resource(resource: FHIR.from_contents(multi_member_match_request), profile_url: 'http://hl7.org/fhir/us/davinci-hrex/StructureDefinition/hrex-parameters-member-match-in')
+        end
+      end
+    end
+
 
   end
 end
