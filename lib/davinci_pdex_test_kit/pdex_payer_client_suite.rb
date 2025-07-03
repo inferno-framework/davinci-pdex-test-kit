0adb67f93c38bc42da1aea19aee89a1e36e8623c
Tom Strassner <tstrassner@mitre.org>
2025-07-03 16:08:30 -0500

    Fix indentation in client suite

--- lib/davinci_pdex_test_kit/pdex_payer_client_suite.rb
+++ lib/davinci_pdex_test_kit/pdex_payer_client_suite.rb
@@ -77,15 +77,15 @@ module DaVinciPDexTestKit
         message.message.match?(/\A\S+: \S+: URL value '.*' does not resolve/)
       end
     end
-    
-requirement_sets(
+
+    requirement_sets(
       {
         identifier: 'hl7.fhir.us.davinci-pdex_2.0.0',
         title: 'Da Vinci Payer Data Exchange (PDex) v2.0.0',
         actor: 'Client'
       }
     )
-    
+
     suite_option  :client_type,
                   title: 'Client Security Type',
                   list_options: [
@@ -106,7 +106,7 @@ requirement_sets(
                       value: PDexClientOptions::UDAP_AUTHORIZATION_CODE
                     }
                   ]
-    
+
     resume_test_route :get, RESUME_PASS_PATH do |request|
       PDexPayerClientSuite.extract_token_from_query_params(request)
     end
@@ -190,10 +190,10 @@ requirement_sets(
       optional
     end
 
-  
+
     # TODO: must support validation
 
-    
+
 
     private
 
